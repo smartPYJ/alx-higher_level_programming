63de1eb2a8b68504beee445b25fcbb94a6c494cb
Smart Monday <uchemonday01@gmail.com>
2022-08-28 21:55:26 +0000

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) == 32:
+            a = 32
+        elif ord(i) in range(65, 91) or ord(i) in range(48, 58):
+            a = ord(i)
+        else:
+            a = ord(i) - 32
+        print(chr(a), end="")
+    print("")
