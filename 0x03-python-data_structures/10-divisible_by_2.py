e8837efb67adb8ec319bceee81bdddebb01dde2c
Smart Monday <uchemonday01@gmail.com>
2022-08-30 14:29:19 +0000

    only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list.append(True) 
+            new_list.append(True)
         elif my_list[i] % 2 != 0:
             new_list.append(False)
     return new_list
