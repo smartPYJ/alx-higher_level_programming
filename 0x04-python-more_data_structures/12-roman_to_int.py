8d98ae1809977290923b00372c7daaa4e8b92582
Smart Monday <uchemonday01@gmail.com>
2022-08-31 21:31:45 +0000

    fixed none error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,19 @@
 def roman_to_int(roman_string):
     decode = []
     roman = {"X": 10, "L": 50, "V": 5, "I": 1, "C": 100, "D": 500, "M": 1000}
-    for j in roman_string:
-        decode.append(roman.get(j))
-    decode.append(0)
-    i = 0
-    sumup = 0
-    while i < len(decode)-1:
-        if decode[i] < decode[i+1]:
-            sumup += (decode[i+1] - decode[i])
+    if type(roman_string) != 'str' or roman_string is None:
+        return 0
+    else:
+        for j in roman_string:
+            decode.append(roman.get(j))
+        decode.append(0)
+        i = 0
+        sumup = 0
+        while i < len(decode)-1:
+            if decode[i] < decode[i+1]:
+                sumup += (decode[i+1] - decode[i])
+                i += 1
+            else:
+                sumup += decode[i]
             i += 1
-        else:
-            sumup += decode[i]
-        i += 1
-    return sumup
+        return sumup
