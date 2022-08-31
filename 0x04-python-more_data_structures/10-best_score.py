c2aa4f84af716178452e14474e929a6e1690271c
Smart Monday <uchemonday01@gmail.com>
2022-08-31 22:47:46 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,12 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+        a_dictionary = {
+            'John': 12,
+            'Bob': 14,
+            'Mike': 14,
+            'Molly': 16,
+            'Adam': 10}
         values = list(a_dictionary.values())
         values.sort()
         for key, value in a_dictionary.items():
