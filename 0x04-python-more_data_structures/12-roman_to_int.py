bfbe4fe707c8f99250cd21b755296097bc7b75a9
Smart Monday <uchemonday01@gmail.com>
2022-08-31 17:47:34 +0000

    Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    decode = []
+    roman = {"X": 10, "L": 50, "V": 5, "I": 1, "C": 100, "D": 500, "M": 1000}
+    for j in roman_string:
+        decode.append(roman.get(j))
+    decode.append(0)
+    i = 0
+    sumup = 0
+    while i < len(decode) - 1:
+        if decode[i] < decode[i+1]:
+            sumup += (decode[i+1] - decode[i])
+        else:
+            sumup += decode[i]
+        i += 1
+    return sumup
