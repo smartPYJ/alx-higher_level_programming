82564fe498d7fabfd6b7ba74a055be865b6aa353
Smart Monday <uchemonday01@gmail.com>
2022-09-01 02:46:52 +0000

    Squareed simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in matrix:
+        for j in i:
+            newlist = [j*j for j in i]
+        new.append(newlist)
+    return new
