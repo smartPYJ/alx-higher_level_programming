221619f474f98e2052a55eee802d8de3066d840d
Smart Monday <uchemonday01@gmail.com>
2022-09-02 17:10:27 +0000

    fixed error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new = []
     for i in matrix:
         if len(i) == 0:
-            return 0
+            continue
         else:
             for j in i:
                 newlist = [j*j for j in i]
