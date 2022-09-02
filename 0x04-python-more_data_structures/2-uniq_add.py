cbd13d9f7199115049b4f94f3f4d683125bd3537
Smart Monday <uchemonday01@gmail.com>
2022-09-02 18:00:00 +0000

    add uniqu

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    for i in my_list:
+        if i in new_list:
+            continue
+        else:
+            new_list.append(i)
+    return sum(new_list)
