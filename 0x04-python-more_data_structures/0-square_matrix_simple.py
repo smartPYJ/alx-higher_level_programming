f7edbf7e09842d7dfb4662497313b8798f54876d
Smart Monday <uchemonday01@gmail.com>
2022-09-02 17:05:36 +0000

    Square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,10 @@
 def square_matrix_simple(matrix=[]):
     new = []
     for i in matrix:
-        for j in i:
-            newlist = [j*j for j in i]
-        new.append(newlist)
+        if len(i) == 0:
+            return 0
+        else:
+            for j in i:
+                newlist = [j*j for j in i]
+            new.append(newlist)
     return new
